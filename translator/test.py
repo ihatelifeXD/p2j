c977f676ef4270b10e319224fe3c20524d52ec40
ihatelifeXD <99435356+ihatelifeXD@users.noreply.github.com>
2023-03-27 17:08:57 +0300

    Update test.py

--- translator/test.py
+++ translator/test.py
@@ -1,643 +1,149 @@
-"""     
-        Written By:
-                Chris Humphreys
-                Email: < chris (--AT--) habitualcoder [--DOT--] com >
-                Jan Weiß
-                Email: < jan (--AT--) geheimwerk [--DOT--] de >
-
-        Copyright 2010 Chris Humphreys
-        Copyright 2012-2013 Jan Weiß
- 
-        This program is free software; you can redistribute it and/or modify
-        it under the terms of the GNU General Public License as published by
-        the Free Software Foundation; either version 3 of the License, or
-        (at your option) any later version.
- 
-        This program is distributed in the hope that it will be useful,
-        but WITHOUT ANY WARRANTY; without even the implied warranty of
-        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-        GNU General Public License for more details.
- 
-        You should have received a copy of the GNU General Public License
-        along with this program.  If not, see <http://www.gnu.org/licenses/>.
- """
-
-from trans2 import * 
-import unittest
-
-class TestSequenceFunctions(unittest.TestCase):
-
-	def setUp(self):
-		self.seq = range(10)
-
-	def test_multi_statements(self):
-		code = '''
-def abc(a, b):
-	c = 1
-	d = f
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('public static void abc(a, b) {\nc = 1;\nd = f;\n}\n', res)
-
-	def test_tuples(self):
-		code = '''
-def abc(a, b):
-	c, d = 1, 2
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('public static void abc(a, b) {\nc, d = 1, 2;\n}\n', res)
-
-	def test_algebra(self):
-		code = '''
-def abc(a, b):
-	c = 1 * (5 + 2) - 3 / 2
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('public static void abc(a, b) {\nc = ((1*(5+2))-(3/2));\n}\n', res)
-
-	def test_class(self):
-		code = '''
-class MyClass(BaseClass):
-	def abc(self, a, b):
-		self.c = a / b;
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('public class MyClass extends BaseClass {\npublic void abc(a, b) {\nthis.c = (a/b);\n}\n}\n', res)
-
-	def test_strings(self):
-		code = '''
-def gdef(self):
-	g = "10"
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('public void gdef() {\ng = "10";\n}\n', res)
-
-
-	def test_multiple_methods_in_class(self):
-		code = '''
-class T():
-
-	stat = "h"
-	s2 = 2
-
-	def met1(self):
-		g = "10"
-	def met2(self):
-		g = "10"
-	def met3(self):
-		g = "10"
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('public class T {\n\nstat = "h";\ns2 = 2;\n\npublic void met1() {\ng = "10";\n}\npublic void met2() {\ng = "10";\n}\npublic void met3() {\ng = "10";\n}\n}\n', res)
-
-
-	def test_multiple_statements_top_level(self):
-		code = '''
-stat = "h"
-s2 = 2
-
-def met1(self):
-	g = "10"
-def met2(self):
-	g = "10"
-def met3(self):
-	g = "10"
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('stat = "h";\ns2 = 2;\n\npublic void met1() {\ng = "10";\n}\npublic void met2() {\ng = "10";\n}\npublic void met3() {\ng = "10";\n}\n', res)
-
-
-	def test_comments(self):
-		code = '''
-#a comment
-"""
-A comment
-"""
-class Abc():
-	#Another comment
-	def __init__(self):
-		a = "hello"
-		b = 'there'
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('// a comment \n/*\nA comment\n*/\npublic class Abc {\n// Another comment \npublic Abc() {\na = "hello";\nb = "there";\n}\n}\n', res)
-
-
-
-	def test_if_else(self):
-		code = '''
-if something: 
-	a=b
-	c=d
-else:
-	e=f
-	g=h
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('if (something) {\na = b;\nc = d;\n} else {\ne = f;\ng = h;\n}\n', res)
-
-	def test_if_elseif(self):
-		code = '''
-if something: 
-	a=b
-	c=d
-elif somethingelse:
-	e=f
-	g=h
-else:
-	z=y
-	x=r
-'''
-
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('if (something) {\na = b;\nc = d;\n} else {\nif (somethingelse) {\ne = f;\ng = h;\n} else {\nz = y;\nx = r;\n}\n}\n', res)
-
-
-	def test_if_complex_condition(self):
-		code = '''
-if (aComplex(b) != 3): 
-	c=d
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('if (aComplex(b) != 3) {\nc = d;\n}\n', res)
-
-
-	def test_call(self):
-		code = '''
-aComplex(b, c, d)
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('aComplex(b, c, d);\n', res)
-
-	def test_notEq_primitive(self):
-		code = '''
-a !=2
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('a != 2;\n', res)
-
-	def test_notEq_object(self):
-		code = '''
-a != "test"
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('!a.equals("test");\n', res)
-
-	def test_Eq_primitive(self):
-		code = '''
-a ==2
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('a == 2;\n', res)
-
-	def test_Eq_object(self):
-		code = '''
-a == "test"
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('a.equals("test");\n', res)
-
-
-	def test_nested_comments(self):
-		code = '''
-"""
-	# another comment
-	pass
-"""
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('/*\n\t# another comment\n\tpass\n*/\n', res)
-
-
-	def test_attribute(self):
-		code = '''
-myobj.mymethod(a, b)
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('myobj.mymethod(a, b);\n', res)
-
-
-	def test_return(self):
-		code = '''
-def a(self):
-	return True
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('public void a() {\nreturn true;\n}\n', res)
-
-	def test_return_no_value(self):
-		code = '''
-def abc(self):
-	return
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('public void abc() {\nreturn;\n}\n', res)
-
-
-	def test_augassign(self):
-		code = '''
-def a():
-	b.x -= b.w
-	b.x += b.w
-	b.x *= b.w
-	b.x /= b.w
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('public static void a() {\nb.x-= b.w;\nb.x+= b.w;\nb.x*= b.w;\nb.x/= b.w;\n}\n', res)
-
-	def test_NotIn(self):
-		code = '''
-ind = 'last_fire_time' not in data
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('ind = !data.contains("last_fire_time");\n', res)
-
-	def test_In(self):
-		code = '''
-ind = 'last_fire_time' in data
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('ind = data.contains("last_fire_time");\n', res)
-
-
-	def test_Subscript(self):
-		code = '''
-def a(self):
-	if 'last_fire_time' not in data:
-		data['last_fire_time'] = time
-		time2 = data['last_fire_time']
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('public void a() {\nif (!data.contains("last_fire_time")) {\ndata.put("last_fire_time", time);\ntime2 = data.get("last_fire_time");\n}\n}\n', res)
-
-
-
-	def test_and(self):
-		code = '''
-a = b and c
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('a = b&&c;\n', res)
-
-	def test_boolean_multiple_statements(self):
-		code = '''
-a = b and c and d or e and b > 1 or d < 2
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('a = b&&c&&d||e&&b>1||d<2;\n', res)
-
-
-	def test_or(self):
-		code = '''
-a = b or c
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('a = b||c;\n', res)
-
-
-	def test_string_slice_no_step(self):
-		code = '''
-current_state[0:3]
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('current_state.subSequence(0, 3);\n', res)
-
-	def test_string_open_ended_slice_no_step(self):
-		code = '''
-current_state[3:]
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('current_state.substring(3);\n', res)
-
-
-	def test_True(self):
-		code = '''
-a = True
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('a = true;\n', res)
-
-	def test_False(self):
-		code = '''
-a = False
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('a = false;\n', res)
-
-
-	def test_replaces_self_with_this(self):
-		code = '''
-self.a = True
-'''		
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('this.a = true;\n', res)
-
-	def test_removes_self_from_arg_list(self):
-		code = '''
-def a(self):
-	pass
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('public void a() {\n;\n}\n', res)
-
-	def test_static_members(self):
-		code = '''
-def a():
-	pass
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('public static void a() {\n;\n}\n', res)
-
-
-	def test_empty_list(self):
-		code = '''
-rects = []
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('rects = new ArrayList();\n', res)
-
-
-	def test_list_strings(self):
-		code = '''
-rects = ['c', 'b', 'd']
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('rects = Arrays.asList({"c", "b", "d"});\n', res)
-
-	def test_list_ints(self):
-		code = '''
-rects = [1, 2, 3]
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('rects = Arrays.asList({1, 2, 3});\n', res)
-
-
-	def test_numeric_mod(self):
-		code = '''
-a = 1 % 2
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('a = (1%2);\n', res)
-
-	def test_string_format(self):
-		code = '''
-a = "blah %s %d" % ("jj", 2)
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('a = String.format("blah %s %d", "jj", 2);\n', res)
-
-	def test_gt(self):
-		code = '''
-a > 0
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('a>0;\n', res)
-
-	def test_gte(self):
-		code = '''
-a >= 0
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('a>=0;\n', res)
-
-
-	def test_lt(self):
-		code = '''
-a < 0
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('a<0;\n', res)
-
-	def test_lte(self):
-		code = '''
-a <= 0
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('a<=0;\n', res)
-
-
-	def test_renames_constructors(self):
-		code = '''
-class A:
-	def __init__(self):
-		pass
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('public class A {\npublic A() {\n;\n}\n}\n', res)
-
-	def test_pass(self):
-		code = '''
-def a():
-	pass
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('public static void a() {\n;\n}\n', res)
-
-
-
-	def test_for(self):
-		code = '''
-for u in c_units:
-	a=b
-	d=e
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('for(u:c_units) {\na = b;\nd = e;\n};\n', res)
-
-
-	def test_print(self):
-		code = '''
-print "hello"
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('System.out.println("hello");\n', res)
-
-
-	def test_staticmethod_ignored(self):
-		code = '''
-@staticmethod
-def a():
-	pass
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('public static void a() {\n;\n}\n', res)
-
-	def test_not(self):
-		code = '''
-if not dead:
-	pass
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('if (!dead) {\n;\n}\n', res)
-
-	def test_none_converted_to_null(self):
-		code = '''
-a = None
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('a = null;\n', res)
-
-
-	def test_inline_comment(self):
-		code = '''
-blah=1 # comment
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('blah = 1; // comment\n', res)
-
-
-	def test_tryexception(self):
-		code = '''
-def a():
-	try:
-		a=b
-		b=c
-	except IOError:
-		return
-	except AA:
-		a=b
-		return
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('public static void a() {\ntry {\na = b;\nb = c;\n}\ncatch (IOError) {\nreturn;\n};\ncatch (AA) {\na = b;\nreturn;\n};\n;\n}\n', res)
-
-
-	def test_tryfinally(self):
-		code = '''
-try:
-	a=b
-	b=c
-finally:
-	a = 0
-	b = 0
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('try {\na = b;\nb = c;\n} finally {\na = 0;\nb = 0;\n};\n', res)
-
-
-	def test_while(self):
-		code = '''
-a = 0
-while a:
-	a=b
-	c=d
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('a = 0;\nwhile a{\na = b;\nc = d;\n};\n', res)
-
-
-	def test_ignores_strings_with_hash_when_processing_comments(self):
-		code = '''
-x = "#" + rs + gs + bs
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('x = ((("#"+rs)+gs)+bs);\n', res)
-
-	def test_ignores_default_arg_values(self):
-		code = '''
-def tick(self, framerate=0):
-	tick = pygame.time.get_ticks()
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('public void tick(framerate) {\ntick = pygame.time.get_ticks();\n}\n', res)
-
-
-	def test_bit_or(self):
-		code = '''
-options = False
-options |= True
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('options = false;\noptions|= true;\n', res)
-
-	def test_bit_and(self):
-		code = '''
-options = False
-options &= True
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('options = false;\noptions&= true;\n', res)
-
-	def test_bit_xor(self):
-		code = '''
-options = False
-options ^= True
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('options = false;\noptions^= true;\n', res)
-
-
-
-	def test_break(self):
-		code = '''
-break
-'''
-		p = Parser(None, None)
-		res = p.parse_to_string(code)
-		self.assertEqual('break;\n', res)
-
-
-if __name__ == '__main__':
-    unittest.main()
-
-
-
+#HW1
+
+print("Please select one:")
+print(' 1.Check-in')
+print(" 2.Check-out")
+print(' 3.Print the totals')
+option=int(input('option no.:'))
+while option<1 or option>3:
+    print('Invalid option')
+    option=int(input('Enter a valid option:'))
+status1= 'Available'
+status2= 'Available'
+status3= 'Available'
+status4= 'Available'
+status5= 'Available'
+Total_Check_out=0
+Total_tax=0
+def op1():
+  global status1
+  global status2
+  global status3
+  global status4
+  global status5
+  global Total_Check_out
+  global Total_tax
+  print(' select one of the available rooms:')
+  print('--------------------------------------------------------------')
+  print('No. \t Category \t Satuts \t Cost/day')
+  print('--------------------------------------------------------------')
+  print('1'  '\t  Single  \t',  status1  ,'\t  QR200')
+  print('2  \t  Single  \t' ,status2 , '\t  QR200')
+  print('3  \t  Double  \t' ,status3 , '\t  QR350')
+  print('4  \t  Double  \t' ,status4,  '\t  QR350')
+  print('5  \t  Suit  \t' ,status5,  '\t  QR650')
+  Choice=int(input('Your choice:'))
+  while Choice<1 or Choice>5:
+      print(" NOT VALID ROOM NUMBER")
+      Choice=int(input('Enter valid room number'))
+  if Choice==1 and status1== 'Available':
+    status1='Rented'
+    print('Room 1 is checked in')
+  elif Choice==2 and status2== 'Available':
+    status2='Rented'
+    print('Room 2 is checked in')
+  elif Choice==3 and status3== 'Available':
+    status3='Rented'
+    print('Room 3 is checked in')
+  elif Choice==4 and status4== 'Available':
+    status4='Rented'
+    print('Room 4 is checked in')
+  elif Choice==5 and status5== 'Available':
+    status5='Rented'
+    print('Room 5 is checked in')
+  else:
+     print('Room not Available ')
+  more_op='y'
+  more_op='Y'
+  while more_op=='y' or more_op=='Y':
+    more_op=input('More operations? [Y/N]')
+    if more_op=='n' or more_op=='N':
+     break
+    while more_op!='y' and more_op!='Y':
+       print("invale input")
+       more_op=input('More operations? [Y/N]')
+    if more_op=='y' or more_op=='Y':
+       print("Please select one:")
+       print(' 1.Check-in')
+       print(" 2.Check-out")
+       print(' 3.Print the totals')
+       option=int(input('option no.:'))
+       if option==1:
+        return op1()
+
+    if option==2:
+       room_number=int(input("Enter room number:"))
+       if  room_number==1 and status1=='Rented':
+          status1='Available'
+          days=float(input('Enter the number of days:'))
+          Check_out1=days*200
+          Tax=0.20*Check_out1
+          Total=Check_out1+Tax
+          Total_Check_out=Total_Check_out+Check_out1
+          Total_tax=Total_tax+Tax
+          print('Room',room_number,'is checked out')
+          print('Rental cost:',' QR',format(Check_out1,'.2f'))
+          print('Tax:', 'QR',format(Tax,'.2f'))
+          print('------------------')
+          print('Total:', 'QR',format(Total,'.2f'))
+       elif room_number==2 and status2=='Rented':
+           status2='Available'
+           days=float(input('Enter the number of days:'))
+           Check_out2= days*200
+           Tax=0.20*Check_out2
+           Total=Check_out2+Tax
+           Total_Check_out=Total_Check_out+Check_out2
+           Total_tax=Total_tax+Tax
+           print('Room',room_number,'is checked out')
+           print('Rental cost:',' QR',format(Check_out2,'.2f'))
+           print('Tax:', 'QR',format(Tax,'.2f'))
+           print('------------------')
+           print('Total:', 'QR',format(Total,'.2f'))
+       elif room_number==3 and status3=='Rented':
+           status3='Available'
+           days=float(input('Enter the number of days:'))
+           Check_out3=days*350
+           Tax=0.20*Check_out3
+           Total=Check_out3+Tax
+           Total_Check_out=Total_Check_out+Check_out3
+           Total_tax=Total_tax+Tax
+           print('Room',room_number,'is checked out')
+           print('Rental cost:',' QR',format(Check_out3,'.2f'))
+           print('Tax:', 'QR',format(Tax,'.2f'))
+           print('------------------')
+           print('Total:', 'QR',format(Total,'.2f'))
+       elif room_number==4 and status4=='Rented':
+           status4='Available'
+           days=float(input('Enter the number of days:'))
+           Check_out4=days*350
+           Tax=0.20*Check_out4
+           Total=Check_out4+Tax
+           Total_Check_out=Total_Check_out+Check_out4
+           Total_tax=Total_tax+Tax
+           print('Room',room_number,'is checked out')
+           print('Rental cost:',' QR',format(Check_out4,'.2f'))
+           print('Tax:', 'QR',format(Tax,'.2f'))
+           print('------------------')
+           print('Total:', 'QR',format(Total,'.2f'))
+       elif room_number==5 and status5=='Rented' :
+           status5='Available'
+           days=float(input('Enter the number of days:'))
+           Check_out5=days*650
+           Tax=0.20*Check_out5
+           Total=Check_out5+Tax
+           Total_Check_out=Total_Check_out+Check_out5
+           Total_tax=Total_tax+Tax
+           print('Room',room_number,'is checked out')
+           print('Rental cost:',' QR',format(Check_out5,'.2f'))
+           print('Tax:', 'QR',format(Tax,'.2f'))
+           print('------------------')
+           print('Total:', 'QR',format(Total,'.2f'))
+       else: 
+         print('This room is not rented')
+    if option==3:
+          print('Total income:','QR',format(Total_Check_out,'.2f'))
+          print('Total tax:','QR',format(Total_tax,'.2f'))
+    
+if option==1:
+  op1()
+  
